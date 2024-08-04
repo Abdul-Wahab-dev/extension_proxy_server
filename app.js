fd1db6026b69a6b7f7d4bddd81e9b3d4e0c761f0
abdulwahab <abdulwahabdev0@gmail.com>
2024-08-04 16:46:27 +0500

    setting up proxy server

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const proxy = require("express-http-proxy");
+const app = express();
+
+// Allow any origin
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", proxy("http://flexisaves.toolefy.com"));
+
+app.listen(3001, () => {
+  console.log("Proxy server running on port 3001");
+});
