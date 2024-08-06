cc107e99d1986cc8e9df221641eb46ce51ad55fc
abdulwahab <abdulwahabdev0@gmail.com>
2024-08-07 00:12:31 +0500

    update proxy to check the console.log

--- app.js
+++ app.js
@@ -14,7 +14,14 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", proxy("http://flexisaves.toolefy.com"));
+app.use(
+  "/",
+  (req, res, next) => {
+    console.log(req.headers);
+    next();
+  },
+  proxy("http://flexisaves.toolefy.com")
+);
 
 app.listen(3001, () => {
   console.log("Proxy server running on port 3001");
